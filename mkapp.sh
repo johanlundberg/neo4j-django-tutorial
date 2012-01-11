3b255fe06ab48f82e5bcfe55c150b6b043570f00
Johan Lundberg <lundberg@nordu.net>
2012-01-11 12:49:54 +0100

    Initial commit.

---
+++ mkapp.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# jbn@nordu.net - 2011
+#
+
+basedir=""
+projname="$1"
+appname="$2"
+
+cd $basedir
+
+if [ -d $projname/$appname ]; then
+        echo "App $appname already exists"
+        exit 1
+fi
+
+cd $basedir/$projname
+echo " ** Creating app $appname in project $projname"
+python manage.py startapp $appname
+echo " ** Moving app to apps folder"
+mv $appname apps
+mkdir -p apps/$appname/templates/$appname
---
+++ mkproj.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+#
+# jbn@nordu.net - 2011
+#
+
+basedir=""
+djangoadmin=""
+projname="$1"
+
+cd $basedir
+
+if [ -d $projname ]; then
+        echo "Project $projname already exists"
+        exit 1
+fi
+
+echo " ** Creating project $projname"
+$djangoadmin startproject $projname
+echo " ** Creating folderstructure"
+cd $basedir/$projname
+mkdir templates
+mkdir static
+mkdir static/img
+mkdir static/css
+mkdir static/js
+mkdir static/upload
+mkdir apps
+echo " ** Creating basic files"
+touch templates/base.html
+touch static/css/style.css
+touch apps/__init__.py
+cd $basedir
+if [ -x /usr/bin/tree ]; then
+        tree $projname
+        echo "--------------------------"
+fi
+echo " ** Project created!"
+
