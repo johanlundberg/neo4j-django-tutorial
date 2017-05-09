6febdbf7500d44b8e7c9d73214984664b31d97d6
Johan Lundberg <lundberg@sunet.se>
2017-05-09 16:02:39 +0200

    Added default app config

---
+++ neo4jtut/neo4japp/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'neo4japp.apps.Neo4jappConfig'
---
+++ neo4jtut/neo4japp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Neo4jappConfig(AppConfig):  # Our app config class
+    name = 'neo4japp'
+    verbose_name = "Neo4japp"
