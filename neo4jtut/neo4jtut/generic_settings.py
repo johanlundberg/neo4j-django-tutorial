df63b75407f0659eb73ef292aae40ee0a852afe4
Johan Lundberg <lundberg@sunet.se>
2017-06-29 15:39:35 +0200

    Added basic console logging

--- neo4jtut/neo4jtut/generic_settings.py
+++ neo4jtut/neo4jtut/generic_settings.py
@@ -98,3 +98,19 @@ TEMPLATES = [
         }
     },
 ]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
